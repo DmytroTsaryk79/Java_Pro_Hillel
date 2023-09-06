d8e102cd90055c8f3cd648093898629c204b17be
Dmitry <131007720+DmytroTsaryk79@users.noreply.github.com>
2023-09-06 15:35:14 +0300

    Update src/main/java/hw/hw5/Obstacle.java
    Co-authored-by: Evgeniy Sokolov <evgeniy.sokolov@gmail.com>

--- src/main/java/hw/hw5/Obstacle.java
+++ src/main/java/hw/hw5/Obstacle.java
@@ -9,5 +9,5 @@ public abstract class Obstacle {
         this.value = value;
     }
 
-    public abstract boolean overcome(Participant Participant);
+    public abstract boolean overcome(Participant participant);
 }
