4136b897ade936f64f33f721149cae9e4ba22bd3
Dmitry <dmitrytsarik@gmail.com>
2023-12-06 15:27:24 +0200

    Generics

--- src/main/java/hw/hw15Generics/Apple.java
+++ src/main/java/hw/hw15Generics/Apple.java
@@ -3,9 +3,9 @@ package hw.hw15Generics;
 class Apple extends Fruit {
     private String variety;
     public Apple(String variety) {
-        this.variety = variety;
         super("Apple", 1.0f);
-    }
+        this.variety = variety;
+         }
 
     public String getVariety() {
         return variety;
