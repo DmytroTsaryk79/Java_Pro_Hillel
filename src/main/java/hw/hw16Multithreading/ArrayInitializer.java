c6ec77161d4cd6f104933a3c5aaa0a6d82233401
Dmitry <dmitrytsarik@gmail.com>
2023-12-18 14:56:21 +0200

    Multithreading (#10)
    * add action
    
    * Multithreading
    
    * Multithreading

---
+++ src/main/java/hw/hw16Multithreading/ArrayInitializer.java
@@ -0,0 +1,55 @@
+package hw.hw16Multithreading;
+
+public class ArrayInitializer {
+
+    public static void init(double[] array) {
+        int n = array.length;
+
+        int middle = n / 2;
+        double[] firstHalf = new double[middle];
+        double[] secondHalf = new double[n - middle];
+
+         for (int i = 0; i < middle; i++) {
+            firstHalf[i] = array[i];
+            secondHalf[i] = array[middle + i];
+        }
+
+        Thread thread1 = new Thread(() -> calculateValues(firstHalf, 0, middle, array));
+        Thread thread2 = new Thread(() -> calculateValues(secondHalf, middle, n, array));
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void calculateValues(double[] sourceArray, int startIndex, int endIndex, double[] targetArray) {
+        for (int i = startIndex; i < endIndex; i++) {
+            targetArray[i] = sourceArray[i - startIndex] *
+                    Math.sin(0.2 + i / 5.0) *
+                    Math.cos(0.2 + i / 5.0) *
+                    Math.cos(0.4 + i / 2.0);
+        }
+    }
+
+    public static void main(String[] args) {
+        int arraySize = 10;
+        double[] array = new double[arraySize];
+        double initialValue = 1.0;
+
+        for (int i = 0; i < arraySize; i++) {
+            array[i] = initialValue;
+        }
+
+        init(array);
+
+        for (double value : array) {
+            System.out.println(value);
+        }
+    }
+}
