f10930ff1956f3f8a7a24d295fa7b0f94bb9cb15
Dmitry <dmitrytsarik@gmail.com>
2023-12-27 13:54:43 +0200

    Lambda (#13)
    * add action
    
    * Lambda

---
+++ src/main/java/hw/hw18Lambda/Hero.java
@@ -0,0 +1,28 @@
+package hw.hw18Lambda;
+
+class Hero {
+    String name;
+    String gender;
+    String eyeColor;
+    String race;
+    String hairColor;
+    double height;
+    String publisher;
+    String skinColor;
+    String alignment;
+    int weight;
+
+    public Hero(String name, String gender, String eyeColor, String race, String hairColor,
+                double height, String publisher, String skinColor, String alignment, int weight) {
+        this.name = name;
+        this.gender = gender;
+        this.eyeColor = eyeColor;
+        this.race = race;
+        this.hairColor = hairColor;
+        this.height = height;
+        this.publisher = publisher;
+        this.skinColor = skinColor;
+        this.alignment = alignment;
+        this.weight = weight;
+    }
+}
